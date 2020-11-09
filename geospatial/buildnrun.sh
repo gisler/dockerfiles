db650d77a08065c2088a40e03c5aaa45654c7e2c
Gerold Hepp <gisler@users.noreply.github.com>
2020-11-09 23:46:03 +0100

    shared folder as argument

--- geospatial/buildnrun.sh
+++ geospatial/buildnrun.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-shared=/srv/data
-
-sudo mkdir $shared
-sudo chmod -R 777 $shared
+sudo mkdir $1
+sudo chmod -R 777 $1
 sudo docker build -t geospatial .
-sudo docker run -d --restart unless-stopped -p 8787:8787 -e PASSWORD=$1 -v $shared:/home/rstudio/shared geospatial
+sudo docker run -d --restart unless-stopped -p 8787:8787 -e PASSWORD=$2 -v $1:/home/rstudio/shared geospatial
