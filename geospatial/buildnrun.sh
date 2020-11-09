7aa74e554bcac5ae74a1b0f8c6b0fc8217206e1c
Gerold Hepp <gisler@users.noreply.github.com>
2020-11-09 23:54:32 +0100

    --rm instead of restart

--- geospatial/buildnrun.sh
+++ geospatial/buildnrun.sh
@@ -3,4 +3,4 @@
 sudo mkdir $1
 sudo chmod -R 777 $1
 sudo docker build -t geospatial .
-sudo docker run -d --restart unless-stopped -p 8787:8787 -e PASSWORD=$2 -v $1:/home/rstudio/shared geospatial
+sudo docker run -d --rm -p 8787:8787 -e PASSWORD=$2 -v $1:/home/rstudio/shared geospatial
