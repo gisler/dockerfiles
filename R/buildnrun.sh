7e444b25c09953fe3fac6afeef9ee54959a65852
Gerold Hepp <gisler@users.noreply.github.com>
2020-11-15 15:25:09 +0100

    added hints on workflow

--- R/buildnrun.sh
+++ R/buildnrun.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# 7z e -pPW geospatial/users/users.7z
+# 7z a -pPW geospatial/users/users.7z users -sdel
+
 mkdir -p $1/shinyapps
 mkdir -p $1/pgdata
 chmod -R 0777 $1
-7z a -t7z -p$3 geospatial/users/users.7z users
 SHARED=$1 PASSWORD=$2 USERS_PASSWORD=$3 docker-compose up --build -d
